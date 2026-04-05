7f4bddffba5659f0279999dfe8c9517c31e54ebb
Yuresh Majhi <yureshmee@gmail.com>
2026-04-05 15:42:54 +0545

    nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
     </View>
   );
 }
@@ -15,3 +15,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
